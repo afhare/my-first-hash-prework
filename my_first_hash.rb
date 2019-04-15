0315f72fbdea4f3b5eb1dee47a45c6a016f3b399
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-15 19:29:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,5 @@
-def my_hash = {
+def my_hash
+  your_hash = {
   "name" => "Alex",
   "city" => "Chicago",
   "height" => "short"
@@ -8,9 +9,9 @@ end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  the_manifest = {
+    
+  }
 
 
 end
